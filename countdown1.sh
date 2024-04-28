f7f609e5edc8f1767a707da5a62a781c763059bc
Christopher Cialone <ccialone0929@gmail.com>
2024-04-28 18:21:09 -0400

    Create countdown1.sh
    Added time dealy with sleep to the countdown

---
+++ countdown1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo -e "\n~~ Countdown Timer ~~\n"
+# Program that counts down to zero from a given argument with time delay
+
+if [[ $1 -gt 0 ]]
+then
+  for (( i = $1; i >= 0; i-- ))
+do
+  echo $i 
+  sleep 1
+done
+else
+  echo Include a positive integer as the first argument.
+fi
